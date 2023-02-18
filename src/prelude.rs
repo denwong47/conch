6bdfe47c1aea0948d3e76519c9bb242bc53de656
Denny Wong <denwong47@hotmail.com>
2023-02-18 11:19:41 +0000

    Added prelude.rs to conch.

---
+++ src/prelude.rs
@@ -0,0 +1,7 @@
+//! A subset of structs, enums and traits that are more likely required at import.
+//!
+//!
+
+pub use conch_ansi::{Background, Colour, Intensity, Modifier};
+
+pub use conch_base_models::{IntoANSIEscapeCode, ModifierError, Resetter, StringWrapper};
