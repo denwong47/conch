9a7a4576a0d9b425832131a75c74b3ab67047c09
Denny Wong <dwong@forestreet.com>
2023-02-28 22:35:31 +0000

    Added docstring with examples to s! macro.

--- conch_macros/src/s.rs
+++ conch_macros/src/s.rs
@@ -1,6 +1,36 @@
+/// A simple macro allowing string literals to be broken across lines without
+/// affecting indentation, like Python strings.
+/// 
+/// All members need to `impl` [`std::fmt::Display`] trait, as they will be passed into
+/// [`format`] macro.
+/// 
+/// Example:
+/// 
+/// ```rust
+/// use conch_macros::s;
+/// 
+/// assert_eq!(
+///     s!(
+///         "The quick brown fox "
+///         "jumps over the "
+///         "lazy dog"
+///     ),
+///     "The quick brown fox jumps over the lazy dog"
+/// );
+/// 
+/// let name = String::from("John Doe");
+/// assert_eq!(
+///     s!(
+///         "Hello, my name is "
+///         name
+///         "! Nice to meet you."
+///     ),
+///     "Hello, my name is John Doe! Nice to meet you."
+/// );
+/// ```
 #[macro_export]
 macro_rules! s {
     ($shead:tt $($sseg:tt)*) => {
-        $shead.to_string() $(+&$sseg.to_string())*
+        format!("{}", $shead) $(+&format!("{}", $sseg))*
     }
 }
