44edbffd7cc83ef0613161257418b6c414e55c1d
Denny Wong <dwong@forestreet.com>
2023-03-01 01:29:18 +0000

    Added Region to CalendarMonth.]

--- conch_calendar/src/structs/calendar.rs
+++ conch_calendar/src/structs/calendar.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
+use std::marker::PhantomData;
 
 use lazy_static::lazy_static;
 
 #[allow(unused_imports)]
 use chrono::{NaiveDate, NaiveWeek, Weekday};
 
+use crate::RegionMarker;
 use conch_ansi::Modifier;
 
 lazy_static! {
@@ -17,8 +19,12 @@ lazy_static! {
 }
 
 /// A struct to display
-pub struct CalendarMonth {
+pub struct CalendarMonth<Region>
+where
+    Region: RegionMarker,
+{
     pub month: NaiveDate,
+    region: PhantomData<Region>,
 
     // Date related presentation settings
     pub week_starts_with: Weekday,
@@ -32,11 +38,15 @@ pub struct CalendarMonth {
     // Decorated Days
     pub decorated_days: HashMap<NaiveDate, Modifier>,
 }
-impl CalendarMonth {
+impl<Region> CalendarMonth<Region>
+where
+    Region: RegionMarker,
+{
     /// Create a new [`CalendarMonth`] from a [`NaiveDate`] provided.
     pub fn new(month: NaiveDate) -> Self {
         Self {
             month,
+            region: PhantomData,
 
             other_month_modifier: DEFAULT_OTHER_MONTH_MODIFIER.clone(),
             week_starts_with: DEFAULT_WEEK_STARTS_WITH.clone(),
--- conch_calendar/src/traits/calendar_day.rs
+++ conch_calendar/src/traits/calendar_day.rs
@@ -1,7 +1,12 @@
-use crate::CalendarMonth;
+use crate::{CalendarMonth, RegionMarker};
 use chrono::{NaiveDate, Weekday};
 
 /// Trait for printing out a day in a Calendar.
-pub(crate) trait DisplayCalendarDay {}
+pub(crate) trait DisplayCalendarDay {
+    fn to_display_on_calendar<Region: RegionMarker>(
+        &self,
+        calendar: &CalendarMonth<Region>,
+    ) -> String;
+}
 
 //
