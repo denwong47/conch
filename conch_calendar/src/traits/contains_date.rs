061c345e1431270c05b8e2d1796fecabe0c3fbf2
Denny Wong <dwong@forestreet.com>
2023-03-07 09:28:01 +0000

    impl ContainsDateMut for Iterator<Item=T: ContainsDate>.

--- conch_calendar/src/traits/contains_date.rs
+++ conch_calendar/src/traits/contains_date.rs
@@ -6,11 +6,27 @@ pub trait ContainsDate {
     fn contains(&self, date: &NaiveDate) -> bool;
 }
 
+/// Trait to check if a date is within another struct of object, presumably a range
+/// of dates.
+pub trait ContainsDateMut {
+    fn contains(&mut self, date: &NaiveDate) -> bool;
+}
+
 impl<'a, T> ContainsDate for Vec<T>
 where
     T: ContainsDate,
 {
     fn contains(&self, date: &NaiveDate) -> bool {
         self.iter().any(|item| item.contains(date))
+    }
+}
+
+impl<'a, I, T> ContainsDateMut for I
+where
+    I: Iterator<Item = T>,
+    T: ContainsDate,
+{
+    fn contains(&mut self, date: &NaiveDate) -> bool {
+        self.any(|item| item.contains(date))
     }
 }
